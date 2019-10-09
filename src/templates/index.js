551e615d1a39338f5ac5f34065ba1e787aac14ef
Wouter <54780752+wouter-dev@users.noreply.github.com>
2019-10-09 18:01:23 +0200

    tadaaa

--- src/templates/index.js
+++ src/templates/index.js
@@ -13,53 +13,9 @@ import { MetaData } from '../components/common/meta'
 * in /utils/siteConfig.js under `postsPerPage`.
 *
 */
-const Index = ({ data, location, pageContext }) => {
-    const posts = data.allGhostPost.edges
-
-    return (
-        <>
-            <MetaData location={location} />
-            <Layout isHome={true}>
-                <div className="container">
-                    <section className="post-feed">
-                        {posts.map(({ node }) => (
-                            // The tag below includes the markup for each post - components/common/PostCard.js
-                            <PostCard key={node.id} post={node} />
-                        ))}
-                    </section>
-                    <Pagination pageContext={pageContext} />
-                </div>
-            </Layout>
-        </>
-    )
-}
-
-Index.propTypes = {
-    data: PropTypes.shape({
-        allGhostPost: PropTypes.object.isRequired,
-    }).isRequired,
-    location: PropTypes.shape({
-        pathname: PropTypes.string.isRequired,
-    }).isRequired,
-    pageContext: PropTypes.object,
-}
-
-export default Index
-
-// This page query loads all posts sorted descending by published date
-// The `limit` and `skip` values are used for pagination
-export const pageQuery = graphql`
-  query GhostPostQuery($limit: Int!, $skip: Int!) {
-    allGhostPost(
-        sort: { order: DESC, fields: [published_at] },
-        limit: $limit,
-        skip: $skip
-    ) {
-      edges {
-        node {
-          ...GhostPostFields
-        }
-      }
-    }
-  }
-`
+<html>
+<header><title>This is title</title></header>
+<body>
+Hello world
+</body>
+</html>
