2b76e9a93f8094f0c753bffbc95e51a86c2cd706
Wouter <54780752+wouter-dev@users.noreply.github.com>
2019-10-09 18:06:42 +0200

    opnieuw

--- src/templates/index.js
+++ src/templates/index.js
@@ -6,14 +6,18 @@ import { Layout, PostCard, Pagination } from '../components/common'
 import { MetaData } from '../components/common/meta'
 
 /**
-* Main index page (home page)
+* Main Index page
 *
 * Loads all posts from Ghost and uses pagination to navigate through them.
 * The number of posts that should appear per page can be setup
 * in /utils/siteConfig.js under `postsPerPage`.
 *
 */
-ReactDOM.render(
+const Index = ({ data, location, pageContext }) => {
+
+  ReactDOM.render(
   <h1>Hello, world!</h1>,
   document.getElementById('root')
 );
+
+export default Index
