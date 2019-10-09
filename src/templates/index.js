dc9b9534f0101c9ebda34458e983b1308462d89d
Wouter <54780752+wouter-dev@users.noreply.github.com>
2019-10-09 18:08:58 +0200

    tja

--- src/templates/index.js
+++ src/templates/index.js
@@ -1,16 +1,8 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
-
-import { Layout, PostCard, Pagination } from '../components/common'
-import { MetaData } from '../components/common/meta'
 
 /**
 * Main Index page
-*
-* Loads all posts from Ghost and uses pagination to navigate through them.
-* The number of posts that should appear per page can be setup
-* in /utils/siteConfig.js under `postsPerPage`.
 *
 */
 const Index = ({ data, location, pageContext }) => {
