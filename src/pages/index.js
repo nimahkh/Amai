270bd85ba170a9b5aea7ef4e73f8d37d051b4692
Wouter <54780752+wouter-dev@users.noreply.github.com>
2019-10-16 16:12:36 +0200

    nu dan pls

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,9 @@
 import React from "react"
 import Layout from "../components/layout"
-import SEO from "../components/seo"
 import Hero from "../components/hero"
 
 const IndexPage = () => (
   <Layout>
-    <SEO />
     <Hero />
   </Layout>
 )
