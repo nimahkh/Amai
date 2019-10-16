4ca9f553b2b1fc037d4fc40759a4b45112e4541f
Wouter <54780752+wouter-dev@users.noreply.github.com>
2019-10-16 15:49:32 +0200

    tja

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,45 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import { StaticQuery } from 'gatsby'
+import BackgroundImage from 'gatsby-background-image'
 
 const IndexPage = () => (
-        <div className="container">
-            <article className="content" style={{ textAlign: `center` }}>
-                <h1 className="content-title">Hello World</h1>
-                <section className="content-body">
-                    Yes you did it
-                </section>
-            </article>
-        </div>
-)
+    <StaticQuery
+      query={graphql`
+        query {
+          desktop: file(relativePath: { eq: "seamless-bg-desktop.jpg" }) {
+            childImageSharp {
+              fluid(quality: 90, maxWidth: 1920) {
+                ...GatsbyImageSharpFluid_withWebp
+              }
+            }
+          }
+        }
+      `}
+      render={data => {
+        // Set ImageData.
+        const imageData = data.desktop.childImageSharp.fluid
+        return (
+          <BackgroundImage
+            Tag="section"
+            className={className}
+            fluid={imageData}
+            backgroundColor={`#040e18`}
+          >
+            <h1>Hello gatsby-background-image</h1>
+          </BackgroundImage>
+        )
+      }}
+    />
+  )
+
+  const StyledBackgroundSection = styled(BackgroundSection)`
+    width: 100%;
+    background-position: bottom center;
+    background-repeat: repeat-y;
+    background-size: cover;
+  `
+
+  export default StyledBackgroundSection
 
 export default IndexPage
