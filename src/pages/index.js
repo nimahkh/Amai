c31bb4c02fd921b935eff897021bf2db57aceb3f
Wouter <54780752+wouter-dev@users.noreply.github.com>
2019-10-16 17:02:52 +0200

    terug

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,8 @@
 import React from "react"
-import Layout from "../components/layout"
-import Hero from "../components/hero"
-import Content from "../components/content"
-import CallToAction from "../components/cta"
+
 
 const IndexPage = () => (
-  <Layout>
-    <Hero />
-    <Content />
-    <CallToAction />
-  </Layout>
+hello world
 )
 
 export default IndexPage
