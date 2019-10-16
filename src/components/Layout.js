50d4d7165cd3455fe500a85fe1da1470e5cbbbb1
Wouter <54780752+wouter-dev@users.noreply.github.com>
2019-10-16 17:09:18 +0200

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -43,15 +43,6 @@ class Layout extends Component {
         `}
         render={data => (
           <>
-            <Helmet
-              title={data.site.siteMetadata.title}
-              meta={[
-                { name: 'description', content: 'Spectral' },
-                { name: 'keywords', content: 'site, web' },
-              ]}
-            >
-              <html lang="en" />
-            </Helmet>
             <div
               className={
                 isPreloaded
