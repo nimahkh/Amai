c6a5fda8834caf25ddfbbd9374302b3a967630d8
Wouter <54780752+wouter-dev@users.noreply.github.com>
2019-10-17 13:57:14 +0200

    mmm

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,8 +2,6 @@ import React from "react"
 import { Link } from "gatsby"
 import styled from "styled-components"
 
-import { rhythm, scale } from "../utils/typography"
-
 class Layout extends React.Component {
   render() {
     const { location, title, children } = this.props
@@ -15,8 +13,7 @@ class Layout extends React.Component {
       header = (
         <h1
           style={{
-            ...scale(1.5),
-            marginBottom: rhythm(1.5),
+            marginBottom: 0,
             marginTop: 0,
           }}
         >
@@ -59,8 +56,8 @@ class Layout extends React.Component {
           style={{
             marginLeft: `auto`,
             marginRight: `auto`,
-            maxWidth: rhythm(24),
-            padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+            maxWidth: 'auto',
+            padding: `10,10,10,10`,
           }}
         >
           <header>{header}</header>
