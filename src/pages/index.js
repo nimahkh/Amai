c84dc54775f26f9d15255f93b2f718161daab568
Wouter <54780752+wouter-dev@users.noreply.github.com>
2019-10-17 13:40:50 +0200

    nu dan?

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import SEO from "../components/seo"
 import Button from "../components/button"
 
 class IndexPage extends React.Component {
@@ -11,10 +10,6 @@ class IndexPage extends React.Component {
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
-        <SEO
-          title="Home"
-          keywords={[`blog`, `gatsby`, `javascript`, `react`]}
-        />
         <img style={{ margin: 0 }} src="./GatsbyScene.svg" alt="Gatsby Scene" />
         <h1>
           Hey people{" "}
