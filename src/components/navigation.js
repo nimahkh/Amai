7b443dd787070dab2da374d22921f72e6b2de93c
Wouter <54780752+wouter-dev@users.noreply.github.com>
2019-10-18 20:44:37 +0200

    Update navigation.js

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -14,27 +14,15 @@ import "./navigation.css";
 class Navigation extends Component {
 
   render() {
-    const whiteBackground = this.state.hasScrolledDown
-      ? "white-background navbar-border"
-      : "";
-    const fontColor = this.state.hasScrolledDown ? "blue-font" : "white-font";
-
     return (
       <Navbar className={whiteBackground} fixed={"top"} expand="md">
         <NavbarBrand href="/" className={fontColor}>
           Wouter
         </NavbarBrand>
-        <NavbarToggler onClick={this.toggleNavbar}>
+        <NavbarToggler>
           <i className={`fa fa-navicon ${fontColor}`} />
         </NavbarToggler>
-        <Collapse isOpen={this.state.isOpen} className={`${fontColor}`} navbar>
-          <Scrollspy
-            items={["about", "blog", "contact"]}
-            currentClassName="active"
-            className={`${fontColor} ml-auto navbar-nav`}
-            navbar
-          >
-            <NavItem onClick={this.handleCloseCollapse}>
+            <NavItem>
               <ScrollchorItem to="#about" className="nav-link">
                 Over mij
               </ScrollchorItem>
@@ -44,7 +32,7 @@ class Navigation extends Component {
                 Blog
               </NavLink>
             </NavItem>
-            <NavItem onClick={this.handleCloseCollapse}>
+            <NavItem>
               <ScrollchorItem to="#contact" className="nav-link">
                 Contact
               </ScrollchorItem>
