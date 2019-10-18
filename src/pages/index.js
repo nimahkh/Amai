780082180aa81f86e8e9cf7cfdb8922ea15f62fd
Wouter <54780752+wouter-dev@users.noreply.github.com>
2019-10-18 21:06:50 +0200

    zonder nav

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,7 @@
 import React from "react";
 import { graphql } from 'gatsby'
 import Cover from "../components/cover";
-import Navigation from "../components/navigation";
 import { Helmet } from "react-helmet";
-import favicon from "../static/logo/favicon.png";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 
