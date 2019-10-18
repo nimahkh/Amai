75e5999362c5d1dbaaaaf8fdad8d51ff6f37dd1b
Wouter <54780752+wouter-dev@users.noreply.github.com>
2019-10-18 20:50:22 +0200

    Update navigation.js

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -8,22 +8,76 @@ import {
   Nav,
   NavItem
 } from "reactstrap";
+import ScrollchorItem from "./scrollchor-item";
 import Scrollspy from "react-scrollspy";
 import "./navigation.css";
 
 class Navigation extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hasScrolledDown: false,
+      isOpen: false
+    };
+    this.handleScroll = this.handleScroll.bind(this);
+    this.toggleNavbar = this.toggleNavbar.bind(this);
+    this.handleCloseCollapse = this.handleCloseCollapse.bind(this);
+  }
+
+  handleScroll() {
+    const bodyScrollTop =
+      document.documentElement.scrollTop || document.body.scrollTop;
+    let scrolledDownEnough = bodyScrollTop > 75 ? true : false;
+    this.setState({
+      hasScrolledDown: scrolledDownEnough
+    });
+  }
+
+  toggleNavbar() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
+  handleCloseCollapse() {
+    if (this.state.isOpen) {
+      this.setState({
+        isOpen: !this.state.isOpen
+      });
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.handleScroll);
+  }
 
   render() {
+    const whiteBackground = this.state.hasScrolledDown
+      ? "white-background navbar-border"
+      : "";
+    const fontColor = this.state.hasScrolledDown ? "blue-font" : "white-font";
+
     return (
       <Navbar className={whiteBackground} fixed={"top"} expand="md">
         <NavbarBrand href="/" className={fontColor}>
           Wouter
         </NavbarBrand>
-        <NavbarToggler>
+        <NavbarToggler onClick={this.toggleNavbar}>
           <i className={`fa fa-navicon ${fontColor}`} />
         </NavbarToggler>
-        <Navbar>
-            <NavItem>
+        <Collapse isOpen={this.state.isOpen} className={`${fontColor}`} navbar>
+          <Scrollspy
+            items={["about", "blog", "contact"]}
+            currentClassName="active"
+            className={`${fontColor} ml-auto navbar-nav`}
+            navbar
+          >
+            <NavItem onClick={this.handleCloseCollapse}>
               <ScrollchorItem to="#about" className="nav-link">
                 Over mij
               </ScrollchorItem>
@@ -33,11 +87,13 @@ class Navigation extends Component {
                 Blog
               </NavLink>
             </NavItem>
-            <NavItem>
+            <NavItem onClick={this.handleCloseCollapse}>
               <ScrollchorItem to="#contact" className="nav-link">
                 Contact
               </ScrollchorItem>
             </NavItem>
+          </Scrollspy>
+        </Collapse>
       </Navbar>
     );
   }
