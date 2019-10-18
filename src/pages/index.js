7a2cdce6c650c0f18b3166297d8cd398d9587408
Wouter <54780752+wouter-dev@users.noreply.github.com>
2019-10-18 21:07:45 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,59 +13,8 @@ const HomePage = ({ data }) => {
         <link rel="icon" type="/image/png" href={favicon} />
       </Helmet>
       <Cover coverImg={data.coverImg} />
-      <Navigation />
     </div>
   );
 };
 
 export default HomePage;
-
-export const query = graphql`
-  query allImgsQuery {
-
-    coverImg: imageSharp(id: { regex: "/AirBalloon/" }) {
-      sizes(maxWidth: 1200) {
-        ...GatsbyImageSharpSizes
-      }
-    }
-
-    profileImg: imageSharp(id: { regex: "/Maribel/" }) {
-      sizes(maxWidth: 420, maxHeight: 630) {
-        ...GatsbyImageSharpSizes
-      }
-    }
-
-    ProjectImgs: allFile(
-      sort: { order: ASC, fields: [absolutePath] }
-      filter: { relativePath: { regex: "/projects/.*.png/" } }
-    ) {
-      edges {
-        node {
-          relativePath
-          name
-          childImageSharp {
-            sizes(maxWidth: 320) {
-              ...GatsbyImageSharpSizes
-            }
-          }
-        }
-      }
-    }
-    HobbyImgs: allFile(
-      sort: { order: ASC, fields: [absolutePath] }
-      filter: { relativePath: { regex: "/icons/.*.png/" } }
-    ) {
-      edges {
-        node {
-          relativePath
-          name
-          childImageSharp {
-            sizes(maxWidth: 40) {
-              ...GatsbyImageSharpSizes
-            }
-          }
-        }
-      }
-    }
-  }
-`;
