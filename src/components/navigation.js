a89a15d6aabbed3f8fb8d2e2977ff1ef83b0534d
Wouter <54780752+wouter-dev@users.noreply.github.com>
2019-10-20 14:02:50 +0200

    Update navigation.js

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -14,54 +14,8 @@ import "./navigation.css";
 import { Link } from 'gatsby'
 
 class Navigation extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      hasScrolledDown: false,
-      isOpen: false
-    };
-    this.handleScroll = this.handleScroll.bind(this);
-    this.toggleNavbar = this.toggleNavbar.bind(this);
-    this.handleCloseCollapse = this.handleCloseCollapse.bind(this);
-  }
-
-  handleScroll() {
-    const bodyScrollTop =
-      document.documentElement.scrollTop || document.body.scrollTop;
-    let scrolledDownEnough = bodyScrollTop > 75 ? true : false;
-    this.setState({
-      hasScrolledDown: scrolledDownEnough
-    });
-  }
-
-  toggleNavbar() {
-    this.setState({
-      isOpen: !this.state.isOpen
-    });
-  }
-
-  handleCloseCollapse() {
-    if (this.state.isOpen) {
-      this.setState({
-        isOpen: !this.state.isOpen
-      });
-    }
-  }
-
-  componentDidMount() {
-    window.addEventListener("scroll", this.handleScroll);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener("scroll", this.handleScroll);
-  }
 
   render() {
-    const whiteBackground = this.state.hasScrolledDown
-      ? "white-background navbar-border"
-      : "";
-    const fontColor = this.state.hasScrolledDown ? "blue-font" : "white-font";
 
     return (
       <Navbar className={whiteBackground} fixed={"top"} expand="md">
