5714e2ddb8bf0afc5e252fd0a65d5feca352c52e
Wouter <54780752+wouter-dev@users.noreply.github.com>
2019-10-20 15:23:22 +0200

    effe proberen

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { graphql } from 'gatsby'
 import Cover from "../components/cover";
-import Navigation from "../components/navigation";
+import Menu from '../components/Menu'
 import { Helmet } from "react-helmet";
 import favicon from "../static/logo/favicon.png";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -15,7 +15,7 @@ const HomePage = ({ data }) => {
         <link rel="icon" type="/image/png" href={favicon} />
       </Helmet>
       <Cover coverImg={data.coverImg} />
-      <Navigation />
+      <Menu />
     </div>
   );
 };
