e32a1e1203cab1027c036b4336eb6b3b2fc447a2
Wouter <54780752+wouter-dev@users.noreply.github.com>
2019-10-24 11:13:41 +0200

    Create poep.js

---
+++ src/pages/poep.js
@@ -0,0 +1,123 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+import favicon from "../static/logo/favicon.png";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const HomePage = () => (
+    <div>
+      <Helmet>
+      <title>Poep</title>
+      <link rel='stylesheet' href='https://s3-us-west-2.amazonaws.com/s.cdpn.io/1462889/unicons.css' />
+      <link rel="stylesheet" href="/style.css" />
+      <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js' />
+      <script  src="/script.js" />
+      </Helmet>
+
+      <div class="hero-center-section">
+      		<div class="left-text">nature</div>
+      		<div class="container">
+      			<div class="row justify-content-center">
+      				<div class="col-md-8">
+      					<div class="img-wrap">
+      						<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1462889/nature.jpg" alt="">
+      					</div>
+      				</div>
+      			</div>
+      		</div>
+      	</div>
+      	<div class="hero-center-section">
+      		<div class="left-text">travel</div>
+      		<div class="container">
+      			<div class="row justify-content-center">
+      				<div class="col-md-8">
+      					<div class="img-wrap">
+      						<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1462889/travel.jpg" alt="">
+      					</div>
+      				</div>
+      			</div>
+      		</div>
+      	</div>
+      	<div class="hero-center-section">
+      		<div class="left-text">blog</div>
+      		<div class="container">
+      			<div class="row justify-content-center">
+      				<div class="col-md-8">
+      					<div class="img-wrap">
+      						<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1462889/fashion.jpg" alt="">
+      					</div>
+      				</div>
+      			</div>
+      		</div>
+      	</div>
+      	<div class="hero-center-section">
+      		<div class="left-text">animals</div>
+      		<div class="container">
+      			<div class="row justify-content-center">
+      				<div class="col-md-8">
+      					<div class="img-wrap">
+      						<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1462889/animals.jpg" alt="">
+      					</div>
+      				</div>
+      			</div>
+      		</div>
+      	</div>
+      	<div class="hero-center-section">
+      		<div class="left-text">business</div>
+      		<div class="container">
+      			<div class="row justify-content-center">
+      				<div class="col-md-8">
+      					<div class="img-wrap">
+      						<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1462889/business.jpg" alt="">
+      					</div>
+      				</div>
+      			</div>
+      		</div>
+      	</div>
+      	<div class="hero-center-section">
+      		<div class="left-text">art</div>
+      		<div class="container">
+      			<div class="row justify-content-center">
+      				<div class="col-md-8">
+      					<div class="img-wrap">
+      						<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1462889/art.jpg" alt="">
+      					</div>
+      				</div>
+      			</div>
+      		</div>
+      	</div>
+
+
+      	<div class="section padding-top-bottom over-hide z-bigger">
+      		<ul class="slide-buttons">
+      			<li class="">
+      				<a href="#0" class="hover-target" data-hover="nature">nature</a>
+      			</li>
+      			<li class="">
+      				<a href="#0" class="hover-target" data-hover="travel">travel</a>
+      			</li>
+      			<li class="">
+      				<a href="/blog" class="hover-target" data-hover="blog">blog</a>
+      			</li>
+      			<li class="">
+      				<a href="#0" class="hover-target" data-hover="animals">animals</a>
+      			</li>
+      			<li class="">
+      				<a href="#0" class="hover-target" data-hover="business">business</a>
+      			</li>
+      			<li class="">
+      				<a href="#0" class="hover-target" data-hover="art">art</a>
+      			</li>
+      		</ul>
+      	</div>
+
+      	<!-- Page cursor
+      	================================================== -->
+
+      	<div class='cursor' id="cursor"></div>
+      	<div class='cursor2' id="cursor2"></div>
+      	<div class='cursor3' id="cursor3"></div>
+
+    </div>
+  );
+
+export default HomePage;
