22aff64c51e3f1028ca4613e127e9319258a974a
Wouter <54780752+wouter-dev@users.noreply.github.com>
2019-10-24 12:29:49 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,6 +10,7 @@ const HomePage = () => (
     <div>
       <Helmet>
       <title>Wouter</title>
+      <script  src="/script.js" />
       <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js' />
       </Helmet>
 
@@ -114,8 +115,6 @@ const HomePage = () => (
       	<div class='cursor' id="cursor"></div>
       	<div class='cursor2' id="cursor2"></div>
       	<div class='cursor3' id="cursor3"></div>
-
-        <script  src="/script.js" />
 
       </body>
     </div>
