a00e6f725a7c96feaa0c37544daf37f4ae5b4867
Wouter <54780752+wouter-dev@users.noreply.github.com>
2019-10-24 15:24:42 +0200

    zal wel fout gaan

---
+++ src/pages/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { graphql } from 'gatsby'
+import Cover from "../components/cover";
+import Navi from '../components/nav'
+import { Helmet } from "react-helmet";
+import favicon from "../static/logo/favicon.png";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+
+const HomePage = () => (
+    <div>
+      <Helmet>
+        <title>Sitee</title>
+        <link rel="icon" type="/image/png" href={favicon} />
+      </Helmet>
+      <Cover />
+      <Navi />
+    </div>
+  );
+
+export default HomePage;
