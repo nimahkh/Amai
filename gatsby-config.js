8ba1fdd67460f27f3e52f2d129f3638643c4b714
Wouter <54780752+wouter-dev@users.noreply.github.com>
2019-10-24 16:02:40 +0200

    offline

--- gatsby-config.js
+++ gatsby-config.js
@@ -182,6 +182,6 @@ module.exports = {
         `gatsby-plugin-catch-links`,
         `gatsby-plugin-react-helmet`,
         `gatsby-plugin-force-trailing-slashes`,
-        `gatsby-plugin-remove-serviceworker`,
+        `gatsby-plugin-offline`,
     ],
 }
