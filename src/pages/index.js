b072e4c2f3cc0573f2aeca5e6f2d7a7e863d3772
Wouter <54780752+wouter-dev@users.noreply.github.com>
2019-10-25 14:27:16 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,60 +3,13 @@ import { Helmet } from "react-helmet";
 import favicon from "../static/logo/favicon.png";
 import { Link } from "gatsby"
 import "../styles/unicons.css"
-import $ from 'jquery';
-
-componentDidMount();
-  if (typeof window !== `undefined`) {
-  (function($) { "use strict";
-    $(document).ready(function() {
-
-  		$('.slide-buttons li:nth-child(1)').on('mouseenter', function() {
-  			$('.slide-buttons li.active').removeClass('active');
-  			$('.hero-center-section.show').removeClass("show");
-  			$('.hero-center-section:nth-child(1)').addClass("show");
-  			$('.slide-buttons li:nth-child(1)').addClass('active');
-  		})
-  		$('.slide-buttons li:nth-child(2)').on('mouseenter', function() {
-  			$('.slide-buttons li.active').removeClass('active');
-  			$('.hero-center-section.show').removeClass("show");
-  			$('.hero-center-section:nth-child(2)').addClass("show");
-  			$('.slide-buttons li:nth-child(2)').addClass('active');
-  		})
-  		$('.slide-buttons li:nth-child(3)').on('mouseenter', function() {
-  			$('.slide-buttons li.active').removeClass('active');
-  			$('.hero-center-section.show').removeClass("show");
-  			$('.hero-center-section:nth-child(3)').addClass("show");
-  			$('.slide-buttons li:nth-child(3)').addClass('active');
-  		})
-  		$('.slide-buttons li:nth-child(4)').on('mouseenter', function() {
-  			$('.slide-buttons li.active').removeClass('active');
-  			$('.hero-center-section.show').removeClass("show");
-  			$('.hero-center-section:nth-child(4)').addClass("show");
-  			$('.slide-buttons li:nth-child(4)').addClass('active');
-  		})
-  		$('.slide-buttons li:nth-child(5)').on('mouseenter', function() {
-  			$('.slide-buttons li.active').removeClass('active');
-  			$('.hero-center-section.show').removeClass("show");
-  			$('.hero-center-section:nth-child(5)').addClass("show");
-  			$('.slide-buttons li:nth-child(5)').addClass('active');
-  		})
-  		$('.slide-buttons li:nth-child(6)').on('mouseenter', function() {
-  			$('.slide-buttons li.active').removeClass('active');
-  			$('.hero-center-section.show').removeClass("show");
-  			$('.hero-center-section:nth-child(6)').addClass("show");
-  			$('.slide-buttons li:nth-child(6)').addClass('active');
-  		})
-  		$('.slide-buttons li:nth-child(1)').trigger('mouseenter')
-
-  	});
-  })(jQuery);
-  }
-}
 
 const HomePage = () => (
     <div>
       <Helmet>
       <title>Wouter</title>
+      <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js' />
+      <script  src="/script.js" />
       <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css' />
       <link rel="stylesheet" href="/style.css" />
       </Helmet>
@@ -122,7 +75,6 @@ const HomePage = () => (
       			</div>
       		</div>
       	</div>
-
 
       	<div class="section padding-top-bottom over-hide z-bigger">
       		<ul class="slide-buttons">
