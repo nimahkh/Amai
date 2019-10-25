f85f5e8b03efd89162de1114f440c03199870ba2
Wouter <54780752+wouter-dev@users.noreply.github.com>
2019-10-25 13:53:48 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,48 +6,6 @@ import styled from 'styled-components'
 import "../styles/unicons.css"
 import $ from 'jquery';
 
-$(document).ready(function() {
-
-  $('.slide-buttons li:nth-child(1)').on('mouseenter', function() {
-    $('.slide-buttons li.active').removeClass('active');
-    $('.hero-center-section.show').removeClass("show");
-    $('.hero-center-section:nth-child(1)').addClass("show");
-    $('.slide-buttons li:nth-child(1)').addClass('active');
-  })
-  $('.slide-buttons li:nth-child(2)').on('mouseenter', function() {
-    $('.slide-buttons li.active').removeClass('active');
-    $('.hero-center-section.show').removeClass("show");
-    $('.hero-center-section:nth-child(2)').addClass("show");
-    $('.slide-buttons li:nth-child(2)').addClass('active');
-  })
-  $('.slide-buttons li:nth-child(3)').on('mouseenter', function() {
-    $('.slide-buttons li.active').removeClass('active');
-    $('.hero-center-section.show').removeClass("show");
-    $('.hero-center-section:nth-child(3)').addClass("show");
-    $('.slide-buttons li:nth-child(3)').addClass('active');
-  })
-  $('.slide-buttons li:nth-child(4)').on('mouseenter', function() {
-    $('.slide-buttons li.active').removeClass('active');
-    $('.hero-center-section.show').removeClass("show");
-    $('.hero-center-section:nth-child(4)').addClass("show");
-    $('.slide-buttons li:nth-child(4)').addClass('active');
-  })
-  $('.slide-buttons li:nth-child(5)').on('mouseenter', function() {
-    $('.slide-buttons li.active').removeClass('active');
-    $('.hero-center-section.show').removeClass("show");
-    $('.hero-center-section:nth-child(5)').addClass("show");
-    $('.slide-buttons li:nth-child(5)').addClass('active');
-  })
-  $('.slide-buttons li:nth-child(6)').on('mouseenter', function() {
-    $('.slide-buttons li.active').removeClass('active');
-    $('.hero-center-section.show').removeClass("show");
-    $('.hero-center-section:nth-child(6)').addClass("show");
-    $('.slide-buttons li:nth-child(6)').addClass('active');
-  })
-  $('.slide-buttons li:nth-child(1)').trigger('mouseenter')
-
-});
-
 const HomePage = () => (
     <div>
       <Helmet>
@@ -146,5 +104,43 @@ const HomePage = () => (
       </body>
     </div>
   );
+
+  $('.slide-buttons li:nth-child(1)').on('mouseenter', function() {
+    $('.slide-buttons li.active').removeClass('active');
+    $('.hero-center-section.show').removeClass("show");
+    $('.hero-center-section:nth-child(1)').addClass("show");
+    $('.slide-buttons li:nth-child(1)').addClass('active');
+  })
+  $('.slide-buttons li:nth-child(2)').on('mouseenter', function() {
+    $('.slide-buttons li.active').removeClass('active');
+    $('.hero-center-section.show').removeClass("show");
+    $('.hero-center-section:nth-child(2)').addClass("show");
+    $('.slide-buttons li:nth-child(2)').addClass('active');
+  })
+  $('.slide-buttons li:nth-child(3)').on('mouseenter', function() {
+    $('.slide-buttons li.active').removeClass('active');
+    $('.hero-center-section.show').removeClass("show");
+    $('.hero-center-section:nth-child(3)').addClass("show");
+    $('.slide-buttons li:nth-child(3)').addClass('active');
+  })
+  $('.slide-buttons li:nth-child(4)').on('mouseenter', function() {
+    $('.slide-buttons li.active').removeClass('active');
+    $('.hero-center-section.show').removeClass("show");
+    $('.hero-center-section:nth-child(4)').addClass("show");
+    $('.slide-buttons li:nth-child(4)').addClass('active');
+  })
+  $('.slide-buttons li:nth-child(5)').on('mouseenter', function() {
+    $('.slide-buttons li.active').removeClass('active');
+    $('.hero-center-section.show').removeClass("show");
+    $('.hero-center-section:nth-child(5)').addClass("show");
+    $('.slide-buttons li:nth-child(5)').addClass('active');
+  })
+  $('.slide-buttons li:nth-child(6)').on('mouseenter', function() {
+    $('.slide-buttons li.active').removeClass('active');
+    $('.hero-center-section.show').removeClass("show");
+    $('.hero-center-section:nth-child(6)').addClass("show");
+    $('.slide-buttons li:nth-child(6)').addClass('active');
+  })
+  $('.slide-buttons li:nth-child(1)').trigger('mouseenter')
 
 export default HomePage;
