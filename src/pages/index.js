c74bda4ee3ef3a94569121a58c11eedc953a9f57
Wouter <54780752+wouter-dev@users.noreply.github.com>
2019-10-25 12:48:08 +0200

    woops

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ const HomePage = () => (
       <Helmet>
       <title>Wouter</title>
       <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css' />
-      <link rel="stylesheet" href="../styles/style.css" />
+      <link rel="stylesheet" href="/style.css" />
       </Helmet>
 
       <Helmet>
