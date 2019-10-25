aa44f8b317ec6132b7141f4aedbfa512d43ef94c
Wouter <54780752+wouter-dev@users.noreply.github.com>
2019-10-25 13:23:54 +0200

    misschien

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,14 +7,13 @@ import "../styles/unicons.css"
 const HomePage = () => (
     <div>
       <Helmet>
+      <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js' />
+      <script  src="/script.js" />
+      </Helmet>
+      <Helmet>
       <title>Wouter</title>
       <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css' />
       <link rel="stylesheet" href="/style.css" />
-      </Helmet>
-
-      <Helmet>
-      <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js' />
-      <script  src="/script.js" />
       </Helmet>
 
       <body>
