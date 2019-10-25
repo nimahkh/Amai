588823e6908b79a2efcf69929f1dbade54ebc1eb
Wouter <54780752+wouter-dev@users.noreply.github.com>
2019-10-25 14:13:46 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,52 @@ import { Helmet } from "react-helmet";
 import favicon from "../static/logo/favicon.png";
 import { Link } from "gatsby"
 import "../styles/unicons.css"
-let $;
-if (typeof window !== `undefined`) {
-  $ = require("jquery");
+import $ from 'jquery';
+
+componentDidMount(){
+  if (typeof window !== `undefined`) {
+    $(document).ready(function() {
+
+  		$('.slide-buttons li:nth-child(1)').on('mouseenter', function() {
+  			$('.slide-buttons li.active').removeClass('active');
+  			$('.hero-center-section.show').removeClass("show");
+  			$('.hero-center-section:nth-child(1)').addClass("show");
+  			$('.slide-buttons li:nth-child(1)').addClass('active');
+  		})
+  		$('.slide-buttons li:nth-child(2)').on('mouseenter', function() {
+  			$('.slide-buttons li.active').removeClass('active');
+  			$('.hero-center-section.show').removeClass("show");
+  			$('.hero-center-section:nth-child(2)').addClass("show");
+  			$('.slide-buttons li:nth-child(2)').addClass('active');
+  		})
+  		$('.slide-buttons li:nth-child(3)').on('mouseenter', function() {
+  			$('.slide-buttons li.active').removeClass('active');
+  			$('.hero-center-section.show').removeClass("show");
+  			$('.hero-center-section:nth-child(3)').addClass("show");
+  			$('.slide-buttons li:nth-child(3)').addClass('active');
+  		})
+  		$('.slide-buttons li:nth-child(4)').on('mouseenter', function() {
+  			$('.slide-buttons li.active').removeClass('active');
+  			$('.hero-center-section.show').removeClass("show");
+  			$('.hero-center-section:nth-child(4)').addClass("show");
+  			$('.slide-buttons li:nth-child(4)').addClass('active');
+  		})
+  		$('.slide-buttons li:nth-child(5)').on('mouseenter', function() {
+  			$('.slide-buttons li.active').removeClass('active');
+  			$('.hero-center-section.show').removeClass("show");
+  			$('.hero-center-section:nth-child(5)').addClass("show");
+  			$('.slide-buttons li:nth-child(5)').addClass('active');
+  		})
+  		$('.slide-buttons li:nth-child(6)').on('mouseenter', function() {
+  			$('.slide-buttons li.active').removeClass('active');
+  			$('.hero-center-section.show').removeClass("show");
+  			$('.hero-center-section:nth-child(6)').addClass("show");
+  			$('.slide-buttons li:nth-child(6)').addClass('active');
+  		})
+  		$('.slide-buttons li:nth-child(1)').trigger('mouseenter')
+
+  	});
+  }
 }
 
 const HomePage = () => (
@@ -106,51 +149,5 @@ const HomePage = () => (
       </body>
     </div>
   );
-
-  componentDidMount(){
-    if (typeof window !== `undefined`) {
-      $(document).ready(function() {
-
-    		$('.slide-buttons li:nth-child(1)').on('mouseenter', function() {
-    			$('.slide-buttons li.active').removeClass('active');
-    			$('.hero-center-section.show').removeClass("show");
-    			$('.hero-center-section:nth-child(1)').addClass("show");
-    			$('.slide-buttons li:nth-child(1)').addClass('active');
-    		})
-    		$('.slide-buttons li:nth-child(2)').on('mouseenter', function() {
-    			$('.slide-buttons li.active').removeClass('active');
-    			$('.hero-center-section.show').removeClass("show");
-    			$('.hero-center-section:nth-child(2)').addClass("show");
-    			$('.slide-buttons li:nth-child(2)').addClass('active');
-    		})
-    		$('.slide-buttons li:nth-child(3)').on('mouseenter', function() {
-    			$('.slide-buttons li.active').removeClass('active');
-    			$('.hero-center-section.show').removeClass("show");
-    			$('.hero-center-section:nth-child(3)').addClass("show");
-    			$('.slide-buttons li:nth-child(3)').addClass('active');
-    		})
-    		$('.slide-buttons li:nth-child(4)').on('mouseenter', function() {
-    			$('.slide-buttons li.active').removeClass('active');
-    			$('.hero-center-section.show').removeClass("show");
-    			$('.hero-center-section:nth-child(4)').addClass("show");
-    			$('.slide-buttons li:nth-child(4)').addClass('active');
-    		})
-    		$('.slide-buttons li:nth-child(5)').on('mouseenter', function() {
-    			$('.slide-buttons li.active').removeClass('active');
-    			$('.hero-center-section.show').removeClass("show");
-    			$('.hero-center-section:nth-child(5)').addClass("show");
-    			$('.slide-buttons li:nth-child(5)').addClass('active');
-    		})
-    		$('.slide-buttons li:nth-child(6)').on('mouseenter', function() {
-    			$('.slide-buttons li.active').removeClass('active');
-    			$('.hero-center-section.show').removeClass("show");
-    			$('.hero-center-section:nth-child(6)').addClass("show");
-    			$('.slide-buttons li:nth-child(6)').addClass('active');
-    		})
-    		$('.slide-buttons li:nth-child(1)').trigger('mouseenter')
-
-    	});
-    }
-  }
 
 export default HomePage;
